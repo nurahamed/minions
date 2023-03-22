e6163974976c73753b87a99e4cfff9c4472a79ef
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-03-23 01:25:08 +0530

    js done

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
 
-btnTranslate.addEventListener("click", function(){
-    console.log("Got clicked")
-})
+var outputDiv = document.querySelector("#output");
+
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL (input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Some went wrong with server please truy after some time ")
+}
+
+function clickHandler(){
+    var inputText =textInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        var translateText = json.contents.translated;
+        outputDiv.innerText = translateText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
